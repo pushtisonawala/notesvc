18ae08d6e1f3c300b3240985ac09ae4ef3b4349b
pushtisonawala <pushtisonawala24@gmail.com>
2026-01-17 02:10:13 +0530

    Refactor Notesservice to include Note struct and update GetNotes method to return notes data

--- services/notes.go
+++ services/notes.go
@@ -1,9 +1,17 @@
 package services
 
 type Notesservice struct{}
+type Note struct{
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+func (n *Notesservice) GetNotes() []Note {
+	data:=[]Note{
+		{Id:1,Name:"Note 1"},
+				{Id:2,Name:"Note 2"},
 
-func (n *Notesservice) GetNotes() string {
-	return "Get request notes"
+	}
+	return data
 }
 
 func (n *Notesservice) CreateNotes() string {
